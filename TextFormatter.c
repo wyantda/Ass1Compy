1c9499395c9e44340255fd01ae21654ca3c1c098
wyantda <49352154+wyantda@users.noreply.github.com>
2019-04-29 00:29:52 -0400

    Please.

--- TextFormatter.c
+++ TextFormatter.c
@@ -9,78 +9,43 @@
 
 char *readFile(FILE *);
 void formatter(FILE *, FILE *, int);
-/*
-//main(int argc, char *argv[])
-//{
 
-	////FILE *ifp, *ofp;
-	
-	
-	////clock_t begin_time, end_time;
-	////double time_used;
+int main(/*int argc, char *argv[]*/) {
 
-	////if (argc < 3) {
-	////  fprintf(stderr,"Not enough arguments\n");
-	////  exit(1);
-	////}
-	////if (!(ifp = fopen(argv[1],"r"))) {
-	////  fprintf(stderr,"Cannot open file %s\n",argv[1]);
-	////  exit(1);
-	////}
-	
-	/if (!(ofp = fopen(argv[2],"w"))) {
-	/  exit(1);
-	/}
-	/  fprintf(stderr,"Cannot open file %s\n",argv[2]);
+    int argc = 50;
+    char *argv = "C:\\Users\\DAW\\CLionProjects\\Ass1\\Shakespere.txt";
+    char *ofp = "C:\\Users\\DAW\\CLionProjects\\Ass1\\output-50.txt";
+    FILE *fp;
+    FILE *op;
+    clock_t begin_time, end_time;
+    double time_used;
 
-	////begin_time = clock();
-	/formatter( ifp, ofp, atoi(argv[3]));
-	////end_time = clock();
-	////time_used = (double)(end_time - begin_time)/CLOCKS_PER_SEC;
-	////fprintf( stderr, "Time usage = %17.13f\n", time_used );
-  	fclose(ifp);
-  	fclose(ofp);
-        */
-//}
-int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        fprintf(stderr,"Not enough arguments\n");
+        exit(1);
+    }
 
-	fprintf(stderr, "%d\n", argc);
-   FILE *fp;
-   FILE *op;
-   char buff[255];
-   char * sp; //string pointer
-   clock_t begin_time, end_time;
-   double time_used;
+    if (!(fp = fopen(argv,"r"))) {
+        fprintf(stderr,"Cannot open file %s\n",argv);
+        exit(1);
+    }
 
-   if (argc < 3) {
-   	fprintf(stderr,"Not enough arguments\n");
-   	exit(1);
-   }
-   if (!(fp = fopen(argv[1],"r"))) {
-   	fprintf(stderr,"Cannot open file %s\n",argv[1]);
-   	exit(1);
-   }
-   if (!(op = fopen(argv[2],"w"))) {   
+    if (!(op = fopen(ofp,"w"))) {
         fprintf(stderr,"Cannot open file %s\n",argv[2]);
-	exit(1);
-   }
+        exit(1);
+    }
 
-   int lw = atoi(argv[2]);       
-   begin_time = clock();         
-   //     //           YOUR CODE HERE .....
-   fprintf(stderr,"ERROR: LIU IS BAD\n");
-   formatter( fp, op, lw);
-   //     //            
-   end_time = clock();
-   time_used = (double)(end_time - begin_time)/CLOCKS_PER_SEC;
-   fprintf( stderr, "Time usage = %17.13f\n", time_used );
-   return 0;
+    begin_time = clock();
+    formatter( fp, op, argc);
+    end_time = clock();
+    time_used = (double)(end_time - begin_time)/CLOCKS_PER_SEC;
+    fprintf( stderr, "Time usage = %17.13f\n", time_used );
+    return 0;
 }
+
 void formatter(FILE *ifp, FILE *ofp, int w) {
     int width = w - 1;
-    fprintf(stderr, "WTF\n");
     char *text = readFile(ifp);
-    fprintf(stderr, "IM SERIOUS\n");
     size_t len;
     len = strlen(text);
     char *word = calloc(50, sizeof(char));
@@ -92,64 +57,61 @@ void formatter(FILE *ifp, FILE *ofp, int w) {
         while(!isspace(text[pos]) && pos < len){
             word[wLen++] = text[pos++];
         }
-	fprintf(stderr, "WOWIE ZOW\n");
+
         if((wLen + lLen) > width && lLen != 0){
             do{
-		fprintf(stderr,"NOLANNN\n");
-		int x;
+                int x;
                 for (x = 0; x < lLen; ++x) {
                     if(line[x] == ' '){
-			int z;
+                        int z;
                         for (z = lLen - 1; z > x - 1; --z) {
                             line[z + 1] = line[z];
                         }
-			fprintf(stderr,"%d %d\n", lLen, x);
-			fprintf(stderr, "NOOOOO\n");
                         lLen++;
                         x++;
                         if(lLen >= width){
-			    fprintf(stderr,"NOOOOWW\n");
                             break;
                         }
                     }
                 }
-            }while(lLen < width);	
-            fprintf(stderr,"NOOWWEEWW\n");
+            }while(lLen < width);
             line[lLen] = '\n';
-            fprintf(stderr,"JUST CHECKING\n");
-            //fputs(line, ofp);
-	    fprintf(stderr,"E\n");
+            fputs(line, ofp);
             line = calloc((size_t) (width * 3), sizeof(char));
             lLen = 0;
-	    fprintf(stderr,"WHAT NOW\n");
         }
         if(lLen == 0 || (wLen + 1 + lLen) < width){
-            if(lLen != 0){	
-		fprintf(stderr, "%*c\n",line[lLen]);
+            if(lLen != 0){
                 line[lLen++] = ' ';
             }
-	    int i;
+            int i;
+
             for (i = 0; i < wLen; ++i) {
                 line[lLen++] = word[i];
-		fprintf(stderr, "%*c\n",line[i]);
             }
         }
         memset(word, 0, sizeof(word));
-	pos++;
+        pos++;
     }
-    fprintf(stderr,"WE MADE IT BOIS\n");
-    //fputs(line, ofp);
+
+    fputs(line, ofp);
+
 }
 
+
+
 char *readFile(FILE *in) {
+
     char *text;
     fseek(in, 0, SEEK_END);
-    fprintf(stderr,"God Why\n");
+    //fprintf(stderr,"God Why\n");
     int len = ftell(in) + 1;
     rewind(in);
     text = malloc(len * 2 * (sizeof(char)));
-	fprintf(stderr,"HOW ABOUT HERE\n");
+    //fprintf(stderr,"HOW ABOUT HERE\n");
     fread(text, sizeof(char), (size_t) len, in);
     text[len] = '\0';
+
     return text;
+
 }
